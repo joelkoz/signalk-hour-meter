ab73d65ed047c204bb43d56e4312b80516b76d67
joelkoz <joel.kozikowski@gmail.com>
2019-10-31 15:54:26 -0400

    Initial web app with webpack support

--- src/mainPage.jsx
+++ src/mainPage.jsx
@@ -1,4 +1,22 @@
-'use strict';
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+// Widgets used by this page
+import { DateTimePicker, DropdownList } from 'react-widgets';
+import ReactTable from "react-table";
+
+// CSS for widgets
+import "react-table/react-table.css";
+import 'react-widgets/dist/css/react-widgets.css';
+
+
+// Localization support...
+import Globalize from 'globalize';
+import globalizeLocalizer from 'react-widgets-globalize';
+import ReactFlagsSelect from 'react-flags-select';
+import 'react-flags-select/css/react-flags-select.css';
+
 
 class MainPage extends React.Component {
 
@@ -6,19 +24,93 @@ class MainPage extends React.Component {
           super(props);
 
           // Setup the React "state" object used by this page...
+          let now = new Date();
           this.state = {
             isLoaded: false,
+            devices: null,
+            currentDevice: null,
             data: null,
             error: null,
+            startDate: new Date(now.getTime() - 30 * 24 * 60 * 60 * 1000),
+            endDate: now
         };
+
+        this.state.country = "US";
+        this.state.locale = this.getLocale(this.state.country);
+
+        this.setLocale(this.state.locale);
+
 
       }
     
       updateData() {
-        // Make an API call to retrieve the data...
-        fetch("/plugins/signalk-hour-meter/api/testData")
+      }
+
+
+      componentDidMount() {
+        fetch("/plugins/signalk-hour-meter/api/devices")
           .then((res) => {
-             let myres = res;
+             return res.json()
+          })
+          .then(
+            (data) => {
+              this.setState({
+                isLoaded: true,
+                error: null,
+                devices: data,
+              });
+            },
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error,
+                devices: null
+              });
+            }
+          )      
+      }
+    
+
+      componentWillUnmount() {
+      }
+  
+
+      onFlagChange(country) {
+        let locale = this.getLocale(country);
+        this.setLocale(locale);
+        this.setState({country, locale });
+      }      
+
+      getLocale(country) {
+        console.log(`Country is ${country}`);
+         if (country.indexOf('-') >= 0) {
+          return country.substr(0,2);
+         }
+         else {
+           return country.toLowerCase();
+         }
+      }
+
+
+      setLocale(locale) {
+        console.log(`Locale is ${locale}`);
+        Globalize.locale(locale);
+        globalizeLocalizer();
+        this.formatNumber = Globalize.numberFormatter({ maximumFractionDigits: 2 });
+        this.formatDate = Globalize.dateFormatter({ datetime: "short" });
+      }
+
+      getHours(secs) {
+        return this.formatNumber(secs / 3600);
+      }
+
+
+
+      fetchHistory() {
+        let sStart = this.state.startDate.toISOString();
+        let sEnd = this.state.endDate.toISOString()
+        fetch(`/plugins/signalk-hour-meter/api/history/${this.state.currentDevice}?start=${sStart}&end=${sEnd}`)
+          .then((res) => {
              return res.json()
           })
           .then(
@@ -36,24 +128,16 @@ class MainPage extends React.Component {
                 data: null
               });
             }
-          )
-      }
+        );
 
-
-      componentDidMount() {
-        this.updateData();
-        // Automatically update the page every 10 seconds...
-        this.interval = setInterval(() => this.updateData(), 10000);        
-      }
-    
-
-      componentWillUnmount() {
-        clearInterval(this.interval);
+        this.setState({
+            data: null
+        });
       }
 
 
       render() {
-        const { isLoaded, data, error } = this.state;
+        var { isLoaded, devices, currentDevice, data, error } = this.state;
 
         if (!isLoaded) {
           return <div>Waiting for response from server...</div>;
@@ -64,13 +148,95 @@ class MainPage extends React.Component {
         else {
           return (
             <div>
+
               <h1>Hour meter</h1>
-              <div className="info">Engine 0 revolutions: {data.engine0Revs}</div>
+              <div className="localization inlineRight" >
+                 <ReactFlagsSelect
+                     countries={["US", "GB", "FR", "DE", "ES", "FI"]}
+                     customLabels={{  "US": "en-US",
+                                      "GB": "en-GB",
+                                      "FR": "fr",
+                                      "DE": "de",
+                                      "ES": "es",
+                                      "FI": "fi" }}
+                     onSelect={ value => this.onFlagChange(value) }
+                     defaultCountry={this.state.country}/>
+              </div>
+
+              <div className="device section">
+                <div className="formLabel">Device</div>
+                <DropdownList 
+                     data={devices}
+                     value={currentDevice} 
+                     onChange={ value => this.setState({currentDevice: value, data: null}) }
+                     className="inlineRight devicePicker" />
+              </div>
+
+          { currentDevice &&
+              <div className="history">
+
+                  <div className="dateRange section">
+                    <div className="formLabel">Show history from</div>
+                    <DateTimePicker 
+                        className="inlineRight datePicker"
+                        value={this.state.startDate}
+                        onChange= { value => this.setState({ startDate: value })} />
+                    <div className="inlineRight formLabel">thru</div>
+                    <DateTimePicker 
+                        className="inlineRight datePicker"
+                        value={this.state.endDate}
+                        onChange= { value => this.setState({endDate: value})} />
+
+                    <button className="inlineRight getButton" onClick={this.fetchHistory.bind(this)}>Get history</button>
+                  </div>
+
+             { data &&
+                  <div className = "section results">
+                     <div className = "section lifeStats">
+                        <div className="formLabel">Life time hours:</div>
+                        <div className="inlineRight">{this.getHours(data.totalRunTime)}</div>
+                     </div>
+                     <div className = "section grid">
+                        <ReactTable
+                          data={data.history}
+                          columns={[
+                                {
+                                  Header: "Start",
+                                  id: "start",
+                                  accessor: h => this.formatDate(new Date(h.start))
+                                },
+                                {
+                                  Header: "End",
+                                  id: "end",
+                                  accessor: h => this.formatDate(new Date(h.end))
+                                },
+                                {
+                                  Header: "Run time",
+                                  id: "runTime",
+                                  accessor: h => this.getHours(h.runTime),
+                                  className: 'colRunTime',
+                                  Footer: (
+                                    <div className="colTotal">
+                                        <strong>Total</strong>
+                                        <div className="inlineRight">{this.getHours(data.historyRunTime)}</div>
+                                    </div>
+                                  )
+                                }
+                              ]}
+                          defaultPageSize={ (history.length > 20 ? 20 : history.length )}
+                          className="-striped -highlight"
+                        />
+                     </div>
+                  </div>
+             }
+              </div>
+          }
             </div>
           );
         }
       }
 }
+
 
 let domContainer = document.querySelector('#mainBody');
 ReactDOM.render(<MainPage />, domContainer);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const GlobalizePlugin = require( "globalize-webpack-plugin" );
+var production = process.env.NODE_ENV === "production";
+module.exports = {
+    entry: {
+        mainPage: './src/mainPage.jsx'
+    },
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: '[name].js',
+        publicPath: '/public/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+                cacheDirectory: true,
+                presets: ['@babel/preset-react', '@babel/preset-env']
+            }
+          }
+       },
+       {
+          test: /\.css$/i,
+          use: [ 
+              { loader: 'style-loader' }, 
+              { loader: 'css-loader' } 
+          ]
+       },
+       {
+          test: /\.(gif|ttf|eot|svg|woff2?)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+               name: '[name].[ext]'
+            }
+          }
+       }       
+      ]
+    },
+    plugins: [
+		new GlobalizePlugin({
+			production: production,
+			developmentLocale: "en",
+			supportedLocales: [ "de", "en", "es", "fr", "fi" ],
+			messages: "messages/[locale].json",
+			output: "i18n/[locale].[chunkhash].js"
+		})
+	]
+  };
